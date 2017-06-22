ad29412697dae9f146cd3fb76b4bcc7a56315001
Thomas Gallagher <thomas@keecker.com>
2017-06-22 17:13:01 +0200

    Add display of total USD amount

--- main.go
+++ main.go
@@ -5,9 +5,18 @@ import "log"
 import "github.com/bitfinexcom/bitfinex-api-go/v1"
 import "os"
 import "os/signal"
+import "strings"
+
+type MyData struct {
+    name string
+    amount float64
+    priceUsd float64
+}
 
 func main() {
-    c := bitfinex.NewClient()
+    key := os.Getenv("BFX_API_KEY")
+    secret := os.Getenv("BFX_API_SECRET")
+    c := bitfinex.NewClient().Auth(key, secret)
 
     // in case your proxy is using a non valid certificate set to TRUE
     c.WebSocketTLSSkipVerify = false
@@ -18,7 +27,11 @@ func main() {
     }
     defer Cleanup(c)
 
-    prices := []float64{0.0, 0.0, 0.0}
+    usd := MyData{name: "USD", amount: 0.0, priceUsd: 1.0}
+    iot := MyData{name: "IOT", amount: 0.0, priceUsd: 0.0}
+    eth := MyData{name: "ETH", amount: 0.0, priceUsd: 0.0}
+    xrp := MyData{name: "XRP", amount: 0.0, priceUsd: 0.0}
+    prices := []MyData{usd, iot, eth, xrp}
 
     book_iotusd_chan := make(chan []float64)
     book_ethusd_chan := make(chan []float64)
@@ -26,10 +39,14 @@ func main() {
     c.WebSocket.AddSubscribe(bitfinex.ChanTicker, bitfinex.IOTUSD, book_iotusd_chan)
     c.WebSocket.AddSubscribe(bitfinex.ChanTicker, bitfinex.ETHUSD, book_ethusd_chan)
     c.WebSocket.AddSubscribe(bitfinex.ChanTicker, bitfinex.XRPUSD, book_xrpusd_chan)
+    go ListenChanTicker(book_iotusd_chan, prices[:], 1)
+    go ListenChanTicker(book_ethusd_chan, prices[:], 2)
+    go ListenChanTicker(book_xrpusd_chan, prices[:], 3)
 
-    go listen(book_iotusd_chan, prices[:], 0)
-    go listen(book_ethusd_chan, prices[:], 1)
-    go listen(book_xrpusd_chan, prices[:], 2)
+    // Private channels
+    dataChan := make(chan bitfinex.TermData)
+    go c.WebSocket.ConnectPrivate(dataChan)
+    go ListenPrivate(dataChan, prices[:])
 
     // SIGINT handler
     sigint_chan := make(chan os.Signal, 1)
@@ -52,10 +69,28 @@ func Cleanup(c *bitfinex.Client) {
     c.WebSocket.Close()
 }
 
-func listen(in chan []float64, prices []float64, index int) {
+func ListenChanTicker(in chan []float64, prices []MyData, index int) {
     for {
         msg := <-in
-        prices[index] = msg[0]
-        fmt.Printf("IOTUSD: %.5f    ETHUSD: %.5f    XRPUSD: %.5f\n", prices[0], prices[1], prices[2])
+        prices[index].priceUsd = msg[0]
+        totalUsd := 0.0
+        for _, p := range prices {
+            totalUsd += p.priceUsd * p.amount
+        }
+        fmt.Printf("IOT: %.5f    ETH: %.5f    XRP: %.5f    ~= %.1f USD\n",
+            prices[1].priceUsd, prices[2].priceUsd, prices[3].priceUsd, totalUsd)
+    }
+}
+
+func ListenPrivate(in chan bitfinex.TermData, prices []MyData) {
+    for {
+        msg := <-in
+        if strings.Contains(msg.Term, "ws") {
+            for i, _ := range prices {
+                if prices[i].name == msg.Data[1] {
+                    prices[i].amount = msg.Data[2].(float64)
+                }
+            }
+        }
     }
 }
