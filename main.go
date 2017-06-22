9d3cbc0b3cce4c731ed28b19804b7d6b1cd15729
Thomas Gallagher <thomas@keecker.com>
2017-06-22 16:12:48 +0200

    Add ETHUSD and XRPUSD price display

--- main.go
+++ main.go
@@ -18,11 +18,18 @@ func main() {
     }
     defer Cleanup(c)
 
+    prices := []float64{0.0, 0.0, 0.0}
+
     book_iotusd_chan := make(chan []float64)
-
+    book_ethusd_chan := make(chan []float64)
+    book_xrpusd_chan := make(chan []float64)
     c.WebSocket.AddSubscribe(bitfinex.ChanTicker, bitfinex.IOTUSD, book_iotusd_chan)
+    c.WebSocket.AddSubscribe(bitfinex.ChanTicker, bitfinex.ETHUSD, book_ethusd_chan)
+    c.WebSocket.AddSubscribe(bitfinex.ChanTicker, bitfinex.XRPUSD, book_xrpusd_chan)
 
-    go listen(book_iotusd_chan, "IOTUSD:")
+    go listen(book_iotusd_chan, prices[:], 0)
+    go listen(book_ethusd_chan, prices[:], 1)
+    go listen(book_xrpusd_chan, prices[:], 2)
 
     // SIGINT handler
     sigint_chan := make(chan os.Signal, 1)
@@ -45,9 +52,10 @@ func Cleanup(c *bitfinex.Client) {
     c.WebSocket.Close()
 }
 
-func listen(in chan []float64, message string) {
+func listen(in chan []float64, prices []float64, index int) {
     for {
         msg := <-in
-        fmt.Println(message, msg[0])
+        prices[index] = msg[0]
+        fmt.Printf("IOTUSD: %.5f    ETHUSD: %.5f    XRPUSD: %.5f\n", prices[0], prices[1], prices[2])
     }
 }
