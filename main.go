c607e63edc1e1b13c5a75c0bbd9754606c145567
Thomas Gallagher <thomas@keecker.com>
2017-06-23 10:47:18 +0200

    Add some colors to the output

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
-import "fmt"
-import "log"
-import "github.com/bitfinexcom/bitfinex-api-go/v1"
-import "os"
-import "os/signal"
-import "strings"
+import (
+    "fmt"
+    "log"
+    "os"
+    "os/signal"
+    "strings"
+    "github.com/fatih/color"
+    "github.com/bitfinexcom/bitfinex-api-go/v1"
+)
 
 type MyData struct {
     name string
     amount float64
     priceUsd float64
+    color *color.Color
 }
+
+func (data *MyData) Print() {
+    data.color.Printf("%s: %.4f", data.name, data.priceUsd)
+}
+
+var greenColor = color.New(color.FgWhite).Add(color.BgGreen)
+var redColor = color.New(color.FgWhite).Add(color.BgRed)
+var neutralColor = color.New(color.FgWhite)
 
 func main() {
     key := os.Getenv("BFX_API_KEY")
@@ -27,10 +39,10 @@ func main() {
     }
     defer Cleanup(c)
 
-    usd := MyData{name: "USD", amount: 0.0, priceUsd: 1.0}
-    iot := MyData{name: "IOT", amount: 0.0, priceUsd: 0.0}
-    eth := MyData{name: "ETH", amount: 0.0, priceUsd: 0.0}
-    xrp := MyData{name: "XRP", amount: 0.0, priceUsd: 0.0}
+    usd := MyData{name: "USD", amount: 0.0, priceUsd: 1.0, color: neutralColor}
+    iot := MyData{name: "IOT", amount: 0.0, priceUsd: 0.0, color: neutralColor}
+    eth := MyData{name: "ETH", amount: 0.0, priceUsd: 0.0, color: neutralColor}
+    xrp := MyData{name: "XRP", amount: 0.0, priceUsd: 0.0, color: neutralColor}
     prices := []MyData{usd, iot, eth, xrp}
 
     book_iotusd_chan := make(chan []float64)
@@ -72,13 +84,27 @@ func Cleanup(c *bitfinex.Client) {
 func ListenChanTicker(in chan []float64, prices []MyData, index int) {
     for {
         msg := <-in
+        // Reset colors
+        for i, _ := range prices {
+            prices[i].color = neutralColor
+        }
+        if msg[0] > prices[index].priceUsd {
+            prices[index].color = greenColor
+        } else {
+            prices[index].color = redColor
+        }
+        // Calculate totalUsd
         prices[index].priceUsd = msg[0]
         totalUsd := 0.0
         for _, p := range prices {
             totalUsd += p.priceUsd * p.amount
         }
-        fmt.Printf("IOT: %.5f    ETH: %.5f    XRP: %.5f    ~= %.1f USD\n",
-            prices[1].priceUsd, prices[2].priceUsd, prices[3].priceUsd, totalUsd)
+        // Print
+        for i := 1; i <= 3; i++ {
+            prices[i].Print()
+            fmt.Printf("    ")
+        }
+        fmt.Printf("~= %.1f USD\n", totalUsd)
     }
 }
 
